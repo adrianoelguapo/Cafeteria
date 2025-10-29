192d87776020412347aa516608d0e182b151394a
adrianoelguapo <ex8zari@gmail.com>
2025-10-29 19:12:43 +0100

    CHORE: Estructuración del código

--- src/main/java/com/akadoblee/cafeteria/MainController.java
+++ src/main/java/com/akadoblee/cafeteria/MainController.java
@@ -27,6 +27,7 @@ public class MainController {
         lblEstado.setText("Simulación en curso...");
 
         new Thread(() -> {
+
             Camarero c1 = new Camarero("Camarero 1", colaClientes);
             Camarero c2 = new Camarero("Camarero 2", colaClientes);
 
@@ -42,58 +43,98 @@ public class MainController {
             c2.start();
 
             monitorRunning = true;
+
             Thread monitor = new Thread(() -> {
+
                 while (monitorRunning) {
+
                     List<String> names = new ArrayList<>();
+
                     for (Cliente cl : colaClientes) {
+
                         names.add(cl.nombre);
+
                     }
+
                     Platform.runLater(() -> listaEsperando.getItems().setAll(names));
-                    try { Thread.sleep(200); } catch (InterruptedException ignored) {}
+
+                    try { 
+
+                        Thread.sleep(200); 
+
+                    } catch (InterruptedException ignored) {}
                 }
             });
+
             monitor.setDaemon(true);
             monitor.start();
 
             // iniciar clientes
             for (Cliente cliente : clientes) {
+
                 cliente.start();
+
             }
 
             // esperar fin de todos los clientes
             for (Cliente cliente : clientes) {
-                try { cliente.join(); } catch (InterruptedException ignored) {}
+
+                try { 
+
+                    cliente.join();
+
+                } catch (InterruptedException ignored) {}
+
             }
 
             monitorRunning = false;
-            try { monitor.join(); } catch (InterruptedException ignored) {}
+
+            try { 
+
+                monitor.join();
+
+            } catch (InterruptedException ignored) {}
 
             // actualizar listas finales
             for (Cliente cliente : clientes) {
+
                 if (cliente.atendido) {
+
                     Platform.runLater(() -> {
                         listaEsperando.getItems().remove(cliente.nombre);
                         listaAtendidos.getItems().add(cliente.nombre);
                     });
+
                 } else {
+
                     Platform.runLater(() -> {
                         listaEsperando.getItems().remove(cliente.nombre);
                         listaSeFueron.getItems().add(cliente.nombre);
                     });
+
                 }
+                
             }
 
             c1.interrupt();
             c2.interrupt();
+
             try {
+
                 c1.join();
                 c2.join();
+
             } catch (InterruptedException ignored) {}
 
             Platform.runLater(() -> {
+
                 lblEstado.setText("Todos los clientes fueron atendidos o se fueron.");
                 btnIniciar.setDisable(false);
+
             });
+
         }).start();
+
     }
+
 }
